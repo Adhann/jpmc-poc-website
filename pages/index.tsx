466183fb12cb27e2b33d263f7500c6067bd8bf26
Ramadhan Nur Cholis Januar <ramadhan.nur@photon.com>
2023-08-18 20:29:08 +0700

    add password prompt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
 import React from "react";
-import {JpmcHeader} from "jpmc-poc-components/dist/atoms"
-import {JpmcLoginForm} from "jpmc-poc-components/dist/molecules"
+import { JpmcHeader } from "jpmc-poc-components/dist/atoms";
+import { JpmcLoginForm } from "jpmc-poc-components/dist/molecules";
 import Link from "next/link";
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 import { useRouter } from "next/router";
 
 export default function App() {
-  const router = useRouter()
+  const router = useRouter();
   const onLogin = () => {
-    router.push("./homepage")
-  }
+    router.push("./homepage");
+  };
+
+  const validatePassword = () => {
+    const password = "jpmcphoton";
+
+    const isAuthenticated = localStorage.getItem("is_authenticated");
+    if (isAuthenticated) return;
+
+    const inputPassword = window.prompt("Masukkan password:");
+    if (inputPassword === password) {
+      localStorage.setItem("is_authenticated", true);
+    } else {
+      window.location.reload();
+    }
+  };
+
+  React.useEffect(() => {
+    validatePassword();
+  }, []);
   return (
     <div className={styles.loginPage}>
       <div className={styles.headerBackground}>
@@ -23,5 +41,3 @@ export default function App() {
     </div>
   );
 }
-
-
